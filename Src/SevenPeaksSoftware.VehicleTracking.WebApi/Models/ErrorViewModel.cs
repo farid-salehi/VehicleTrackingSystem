6e572580d09b0f22031d1a00ab41f0a0b287fb8c
f.salehi <7fsalehi@gmail.com>
2020-01-27 07:23:24 +0330

    #0001 initialize the project structure

---
+++ Src/SevenPeaksSoftware.VehicleTracking.WebApi/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SevenPeaksSoftware.VehicleTracking.WebApi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
