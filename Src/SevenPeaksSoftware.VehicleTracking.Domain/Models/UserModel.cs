aeec382117d09e4f6eabc10eb29c8c5320808958
f.salehi <7fsalehi@gmail.com>
2020-01-27 08:17:02 +0330

    #0002 add models.

---
+++ Src/SevenPeaksSoftware.VehicleTracking.Domain/Models/UserModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace SevenPeaksSoftware.VehicleTracking.Domain.Models
+{
+    public class UserModel : BaseModel
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; } 
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; } = "";
+        public string MobileNumber { get; set; } = "";
+        public byte[] Salt { get; set; }
+        public string Password { get; set; }
+        public string RefreshToken { get; set; }
+        public virtual ICollection<UserRoleModel> UserRoleList { get; set; }
+            = new List<UserRoleModel>();
+    }
+}
---
+++ Src/SevenPeaksSoftware.VehicleTracking.Domain/Models/UserRoleModel.cs
@@ -0,0 +1,13 @@
+﻿
+namespace SevenPeaksSoftware.VehicleTracking.Domain.Models
+{
+    public class UserRoleModel: BaseModel
+    {
+        public int UserRoleId { get; set; }
+        public int UserId { get; set; }
+        public virtual UserModel UserInfo { get; set; }
+        public int RoleId { get; set; }
+        public virtual RoleModel RoleInfo { get; set; }
+
+    }
+}
